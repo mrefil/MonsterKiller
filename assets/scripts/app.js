a5c0ee14b95ed7225a460220f3c5c6340ae5453b
Ubuntu <muefiloglu@protonmail.com>
2020-10-26 13:40:50 +0000

    Add Player and Monster Attack value and decrease bar

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
-const ATTACK_VALUE = 9;
+const ATTACK_VALUE = 10;
+const M_ATTACk_VALUE = 10;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth =  chosenMaxLife;
@@ -9,6 +10,16 @@ adjustHealthBars(chosenMaxLife);
 function attackHendler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    const pDamage = dealPlayerDamage(M_ATTACk_VALUE);
+    currentPlayerHealth -= pDamage;
+    
+    if (currentMonsterHealth <=0 && currentPlayerHealth > 0) {
+        alert("You win!");
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth >0) {
+        alert("You lost!");
+    } else if (currentMonsterHealth <=0 && currentPlayerHealth <= 0) {
+        alert("Draw");
+    }
 }
 
 attackBtn.addEventListener('click', attackHendler);
