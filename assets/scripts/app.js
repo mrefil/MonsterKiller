bc52da0f3b9122bb9aa423e84fda5e434e643591
Ubuntu <muefiloglu@protonmail.com>
2020-10-26 15:09:03 +0000

    Ability adjust health value and add logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,14 @@ const M_ATTACk_VALUE = 10;
 const STRONG_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Max Health for you and monster.', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <=0) {
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth =  chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
