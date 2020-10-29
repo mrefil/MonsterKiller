71c745e5b9713a1b6a4bdc06779439741bcd9961
Ubuntu <muefiloglu@protonmail.com>
2020-10-29 15:46:52 +0000

    For-of loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -197,7 +197,17 @@ function printLogHandler() {
 //       i--;
 //       console.log(i);
 //   }
-  console.log(battleLog);
+//   for (let i = 0; i < battleLog.length; i++) {
+//       console.log(battleLog[i]);
+//   }
+  // works only arrays. This will not give indez so we need to add with i variable.
+  let i = 0;
+  for (const logEntry of battleLog) {
+      console.log(logEntry);
+      console.log(i);
+      i++;
+  }
+  
 }
 
 attackBtn.addEventListener('click', attackHendler);
