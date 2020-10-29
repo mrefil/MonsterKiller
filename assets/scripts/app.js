8fbc30e9a3100028c3de5755d1e8e230b9d64f14
Ubuntu <muefiloglu@protonmail.com>
2020-10-29 18:46:53 +0000

    try-catch to Catch &

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -24,7 +24,16 @@ function getMaxLifeValues() {
     return parsedValue;
 }
 
-let chosenMaxLife = getMaxLifeValues();
+let chosenMaxLife;
+
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
+    chosenMaxLife = 100;
+    alert('you entered wrong, default 100');
+}
+
 
 let currentMonsterHealth =  chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
